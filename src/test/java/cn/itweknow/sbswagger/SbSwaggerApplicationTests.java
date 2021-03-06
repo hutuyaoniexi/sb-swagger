d700d70b553747e0eb619b31bfaeb68717490840
Adair <gd8085y@gmail.com>
2021-03-06 19:36:16 +0800

    初始化提交

---
+++ src/test/java/cn/itweknow/sbswagger/SbSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.itweknow.sbswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbSwaggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
